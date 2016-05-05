13131d3e0febb46db8da4f9a6749b6119805095c
Ðani <dguerrab@gmail.com>
2016-05-05 23:13:45 +0200

    Tests DAO, randomizer y asignador
    * Puesta a punto de exclusiones en sorteo básico
    
    * Funcionamiento del Trololo en sorteo avanzado
    
    * Trazas quitadas
    
    * Aviso cuando se introducen dos nombres o email iguales
    
    * Test DAO, funciones randomizer y asignador

---
+++ test/es/upm/dit/isst/amigos/dao/UserDAOImplTest.java
@@ -0,0 +1,56 @@
+package es.upm.dit.isst.amigos.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+import es.upm.dit.isst.amigos.model.User;
+
+public class UserDAOImplTest {
+
+	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+	
+	@Before
+	public void setUp() {
+		helper.setUp();
+	}
+
+	@After
+	public void tearDown() {
+		helper.tearDown();
+    }
+
+	@Test
+	public void testInsertUser() {
+		UserDAOImpl userdao = UserDAOImpl.getInstance();
+		User user = userdao.insertUser("Pepe", "pepe666", "");
+		assertEquals(user.getNick(), "Pepe");
+		assertEquals(user.getEmail(), "pepe666");
+		assertEquals(user.getPerfilsocial(), "");
+	}
+
+	@Test
+	public void testGetUserByNick() {
+		UserDAOImpl userdao = UserDAOImpl.getInstance();
+		User user = userdao.insertUser("Pepe", "pepe666", "");
+		user = userdao.insertUser("Juan", "juanito69", "");
+		assertEquals(userdao.getUserByNick("Pepe").getNick(), "Pepe");
+		assertEquals(userdao.getUserByNick("Pepe").getEmail(), "pepe666");
+		assertEquals(userdao.getUserByNick("Pepe").getPerfilsocial(), "");
+	}
+
+	@Test
+	public void testGetUserByEmail() {
+		UserDAOImpl userdao = UserDAOImpl.getInstance();
+		User user = userdao.insertUser("Pepe", "pepe666", "");
+		user = userdao.insertUser("Juan", "juanito69", "");
+		assertEquals(userdao.getUserByEmail("pepe666").getNick(), "Pepe");
+		assertEquals(userdao.getUserByEmail("pepe666").getEmail(), "pepe666");
+		assertEquals(userdao.getUserByEmail("pepe666").getPerfilsocial(), "");
+	}
+}
