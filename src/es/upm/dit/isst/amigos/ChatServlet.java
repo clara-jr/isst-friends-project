5fa48bb0e97e3505907dcd07c57ce2cfb0e1d645
Lykaos <dragon11_r@hotmail.com>
2016-05-06 16:38:20 +0200

    Correccion de la correcion

--- src/es/upm/dit/isst/amigos/ChatServlet.java
+++ src/es/upm/dit/isst/amigos/ChatServlet.java
@@ -37,18 +37,14 @@ public class ChatServlet extends HttpServlet {
 			User usuario = UserDAOImpl.getInstance().getUserByEmail(userservice.getCurrentUser().getEmail());
 			List<Agrupaciones> agrupuser = AgrupacionesDAOImpl.getInstance().getAgrupacionesByUser(usuario.getNick());
 			List<Grupo> grupos = new ArrayList<Grupo>();
-			HashMap<Long, Agrupaciones[] > agrupacionesporgrupo = new HashMap<Long, Agrupaciones[] >();
-			
 	
 			for (Agrupaciones temp: agrupuser){
-				grupos.add(GrupoDAOImpl.getInstance().getGrupoById(temp.getGrupo()));
-				List<Agrupaciones> agrupacionesdelgrupo = AgrupacionesDAOImpl.getInstance().getAgrupacionesByGrupo(temp.getGrupo());
-				agrupacionesporgrupo.put(temp.getGrupo(), agrupacionesdelgrupo.toArray(new Agrupaciones[agrupacionesdelgrupo.size()]));
+				if (temp.getAmigoinv() != "")
+					grupos.add(GrupoDAOImpl.getInstance().getGrupoById(temp.getGrupo()));
 			}
 			
 			req.getSession().setAttribute("usuario", usuario);
 			req.getSession().setAttribute("grupos", grupos);
-			req.getSession().setAttribute("agrupaciones", agrupacionesporgrupo);
 						
 			resp.sendRedirect("chat.jsp");		
 			
